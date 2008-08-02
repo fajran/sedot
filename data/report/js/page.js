17dfd878202d83ef57b06f9f92ce4fd2a9382638
Fajran Iman Rusadi <fajran@gmail.com>
2008-08-02 16:06:00 +0700

    - Remove jQuery UI (Tabs)

--- data/report/js/page.js
+++ data/report/js/page.js
@@ -1,5 +1,29 @@
 
 window.onload = function() {
-	$('#menu ul').tabs();
+	tabs('#menu');
+}
+
+function tabs(id) {
+	var objs = $(id).children("ul").children("li");
+	objs.each(function(i) {
+		var obj = $(this).children("a");
+		var attr = obj.attr("href");
+		console.log(attr);
+		if (i > 0) {
+			$(attr).addClass("hide");
+		}
+		obj[0].onclick = toggle;
+	});
+}
+
+function toggle() {
+	var attr = $(this).attr("href");
+	$(this).parent().siblings().each(function(i) {
+		var a = $(this).children("a").attr("href")
+		if (!$(a).hasClass("hide")) {
+			$(a).addClass("hide");
+		}
+	});
+	$(attr).removeClass("hide");
 }
 
